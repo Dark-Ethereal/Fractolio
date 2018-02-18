27087e0df11323e58d668257c2714b31c8881606
Dark-Ethereal <darkethereal@googlemail.com>
2018-02-18 10:45:46 +0000

    refactored, addded more color!

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,10 +5,13 @@ module Lib
 import           Codec.Picture
 import           Codec.Picture.Saving (imageToPng)
 import           Codec.Picture.Types
+import           Control.Monad        (liftM2)
 import qualified Data.ByteString.Lazy as B
 import           Data.Complex
+import           Data.Fixed           (mod')
 import           Data.Semigroup       ((<>))
 import           Options.Applicative
+
 
 data View = View
   { coords     :: (Double, Double)
@@ -16,6 +19,8 @@ data View = View
   , resolution :: (Int, Int)
   , iters      :: Int
   }
+
+type Iters = Int
 
 viewParser :: Parser View
 viewParser = View
@@ -30,7 +35,7 @@ viewParser = View
       ( short 'z'
       <> metavar "FACTOR"
       <> help "Set the zoom factor"
-      <> value 0.5
+      <> value 1
       <> showDefault
       )
   <*> argument auto (metavar "RESOLUTION")
@@ -41,27 +46,66 @@ viewParser = View
       <> value 64
       )
 
-mandel :: Int -> Complex Double -> Int
-mandel maxIter = length . takeWhile ((2 >=) . magnitude) . take maxIter . z
+mandel :: RealFloat a =>
+     Iters -- maximum iters
+  -> Complex a -- c value
+  -> Iters -- iterations taken to escape
+mandel maxIter = length . cullEscaped . take maxIter . mPath
   where
-    z c = iterate (\x -> x^2 + c) 0
+    mandelFunc :: RealFloat a => Complex a -> Complex a -> Complex a
+    mandelFunc c = (+c) . (^2)
+
+    mPath :: RealFloat a => Complex a -> [Complex a]
+    mPath = (`iterate` 0) . mandelFunc
+
+    cullEscaped = takeWhile ((2 >=) . magnitude)
 
 mkImage :: View -> Image PixelRGB8
 mkImage v = uncurry (generateImage (imfunc v)) $ resolution v
 
 imfunc :: View -> Int -> Int -> PixelRGB8
-imfunc v x y = mandelbrotColor (iters v) (pixToComplex v x y)
+imfunc v x y = mandelbrotColor (iters v) (pixelToComplex v x y)
 
-pixToComplex :: View -> Int -> Int -> Complex Double
-pixToComplex (View (cx, cy) z (rx,ry) _) x y = nx :+ ny
+pixelToComplex :: View -> Int -> Int -> Complex Double
+pixelToComplex (View (cx, cy) z (rx,ry) _) x y = real :+ complex
   where
-    nx = subtract cx . (* scaler) . subtract ((fromIntegral rx)/2) . fromIntegral $ x
-    ny = subtract cy . (* scaler) . subtract ((fromIntegral ry)/2) . fromIntegral $ y
-    scaler = (2/(fromIntegral ry))*2.5*z
+    real = subtract cx . scale . subtract ((fromIntegral rx)/2) . fromIntegral $ x
+    complex = subtract cy . scale . subtract ((fromIntegral ry)/2) . fromIntegral $ y
+    scale = (/ (fromIntegral ry * z)) . (2.5 *)
 
-mandelbrotColor ::Int -> Complex Double -> PixelRGB8
-mandelbrotColor i c = PixelRGB8 m m m
-  where m = if mandel i c < i then fromIntegral $ (mandel i c) * 255 `div` i else 0
+mandelbrotColor :: RealFloat a =>
+     Iters -- maximum iterations
+  -> Complex a -- initial c value
+  -> PixelRGB8 -- output color
+mandelbrotColor = liftM2 (.) iterColor mandel
+
+iterColor ::
+     Iters -- maximum iterations
+  -> Iters -- iterations taken
+  -> PixelRGB8 -- output color
+iterColor m i
+  | m <= i    = PixelRGB8 0 0 0
+  | otherwise = PixelRGB8 r g b
+  where
+    a :: Double
+    a = fromIntegral i / fromIntegral m
+    r = (h . f) a
+    g = h . f $ a + 1/3
+    b = h . f $ a + 2/3
+
+    f :: Double -> Double
+    f i
+      | mi >= 5/6 = 1
+      | mi >= 2/3 = (i - 2/3) * 6
+      | mi >= 2/3 = 0
+      | mi >= 1/6 = 1 - ((i - 1/6) * 6)
+      | otherwise = 1
+      where mi = mod' i 1
+
+    h = round . (*255)
+
+
+
 
 opts = info (viewParser <**> helper)
   ( fullDesc
